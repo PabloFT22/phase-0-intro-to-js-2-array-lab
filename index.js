5cc4162a198ec87d8ae734602cf7531b125bc18a
Pablo Fuentes Tudela <pablo.fuentes.tudela@outlook.com>
2022-09-05 17:45:09 -0400

    commit tree ed7938c22ae77c83bd8da66bcf9ff24123309e28

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+function prependCat(name){
+    const copyOfCopyOfCats = [name, ...cats];
+    return copyOfCopyOfCats
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
